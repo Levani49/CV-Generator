86fc53788706e3fda2d214cb2381f37b107917fa
Levani49 <levani.pachuashvili.1@btu.edu.ge>
2023-02-09 12:33:48 +0400

    feat: second page's markup side is done

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const secondPageButton = document.getElementById("add-resume");
 const firstPageButton = document.getElementById("first");
+const leftVectorButton = document.getElementById("eclipse");
 
 if (document.title === "First Page") {
   secondPageButton.addEventListener("click", () => {
     window.location.href = "second.html";
   });
 } else if (document.title === "Second Page") {
-  firstPageButton.addEventListener("click", () => {
+  leftVectorButton.addEventListener("click", () => {
     window.location.href = "index.html";
   });
 }
